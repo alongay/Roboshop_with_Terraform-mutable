dbe29a7bb675b603c8ef2cebcb81041e31e14057
Kwasi Alongay <alongay@yahoo.com>
2021-11-14 18:31:40 -0500

    routes.tf

--- vpc/routes.tf
+++ vpc/routes.tf
@@ -4,6 +4,17 @@ resource "aws_route_table" "routes" {
   route {
     cidr_block = "172.31.0.0/16"
     vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+    carrier_gateway_id           = ""
+    destination_prefix_list_id = ""
+    egress_only_gateway_id     = ""
+    gateway_id                 = ""
+    instance_id                = ""
+    ipv6_cidr_block            = ""
+    local_gateway_id           = ""
+    nat_gateway_id             = ""
+    network_interface_id       = ""
+    transit_gateway_id         = ""
+    vpc_endpoint_id            = ""
   }
 
   tags = {
