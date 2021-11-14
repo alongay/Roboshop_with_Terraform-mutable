4116756b3e7f900174052faa961144ae4c422407
Kwasi Alongay <alongay@yahoo.com>
2021-11-14 17:34:11 -0500

    AZS setups

--- vpc/subnets.tf
+++ vpc/subnets.tf
@@ -1,10 +1,11 @@
 resource "aws_subnet" "main" {
-  depends_on = [aws_vpc_ipv4_cidr_block_association.addon]
-  count       = length(var.SUBNETS)
-  vpc_id      = aws_vpc.main.id
-  cidr_block  = element(var.SUBNETS,count.index)
+  depends_on            = [aws_vpc_ipv4_cidr_block_association.addon]
+  count                 = length(var.SUBNETS)
+  vpc_id                = aws_vpc.main.id
+  cidr_block            = element(var.SUBNETS,count.index)
+  availability_zone     = element(var.AZS, count.index)
 
   tags = {
-    Name      = "subnet-${count.index}"
+    Name                = "subnet-${count.index}"
   }
 }
