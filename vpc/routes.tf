5912789819d57da5d1784e2c7e10bd362511ca3d
Kwasi Alongay <alongay@yahoo.com>
2021-11-14 19:01:35 -0500

    routes.tf

--- vpc/routes.tf
+++ vpc/routes.tf
@@ -12,7 +12,6 @@ resource "aws_route" "r" {
   route_table_id            = aws_route_table.route.id
   destination_cidr_block    = var.DEFAULT_VPC_CIDR
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
-  ]
 }
 
 #{
